ad06df714418a93974d90d39cbe75f1afc841255
Beck Grah <jgrah@ucsc.edu>
2026-04-19 15:10:01 -0700

    Remove old cat spritesheets and update sprite loading in the script

--- script.js
+++ script.js
@@ -25,24 +25,45 @@ class Logo extends Phaser.Scene {
     preload() {
         // loads the cat image
         this.load.image('Moji_Pixel 3.png', './assets/Moji_Pixel 3.png');
-        this.load.spritesheet("Moji Pixel Walking Spritesheet.png", "./assets/Moji Pixel Walking Spritesheet.png", { frameWidth: 32, frameHeight: 32 });
-        this.load.spritesheet("Moji Pixel Idle Spritesheet.png", "./assets/Moji Pixel Idle Spritesheet.png", { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('Moji_Pixel_Walking_Spritesheet.png', "./assets/Moji_Pixel_Walking_Spritesheet.png", { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet("Moji_Pixel_Idle_Spritesheet.png", "./assets/Moji_Pixel_Idle_Spritesheet.png", { frameWidth: 32, frameHeight: 32 });
     }
 
     create() {
-        // adds the cat to the scene, starting off-screen to the left
-        this.Moji = this.add.image(-300, 300, 'Moji_Pixel 3.png');
-        this.Moji.setScale(0.5);
+        // adds Moji (the cat) to the scene, starting off-screen to the left
+        this.MojiWalking = this.add.sprite(-300, 300, 'Moji_Pixel_Walking_Spritesheet.png');
+        this.MojiWalking.setScale(10);
+        this.MojiIdle = this.add.sprite(400, 300, 'Moji_Pixel_Idle_Spritesheet.png');
+        this.MojiIdle.setScale(10);
+        this.anims.create({
+            key: 'Moji_Walk_Animation',
+            frames: this.anims.generateFrameNumbers('Moji_Pixel_Walking_Spritesheet.png', { start: 0, end: 11 }),
+            frameRate: 3,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'Moji_Idle_Animation',
+            frames: this.anims.generateFrameNumbers('Moji_Pixel_Idle_Spritesheet.png', { start: 0, end: 7 }),
+            frameRate: 3,
+            repeat: -1
+        });
+        this.MojiIdle.setVisible(false);
+        this.MojiWalking.play('Moji_Walk_Animation');
         this.tweens.add({
-            targets: this.Moji,
-            x: 300,
+            targets: this.MojiWalking,
+            x: 400,
             duration: 2000,
             ease: 'Sine.Out'
         })
     }
 
     update() {
-
+        if (this.MojiIdle.visible == false && this.MojiWalking.x >= 400) {
+            this.MojiWalking.stop();
+            this.MojiIdle.setVisible(true);
+            this.MojiIdle.play('Moji_Idle_Animation');
+            this.MojiWalking.setVisible(false);
+        }
     }
 }
 
@@ -52,6 +73,11 @@ let config = {
     height: 600,
     backgroundColor: 0x729482,
     scene: [Logo],
+    // code to fix render settings to work for pixel art
+    render: {
+        pixelArt: true,
+        antialias: false
+    },
 }
 
 let game = new Phaser.Game(config);
